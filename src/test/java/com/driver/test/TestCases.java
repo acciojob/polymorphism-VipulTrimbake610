163c8db9fe509d08a48023f306e4f4f30498eeb7
VipulTrimbake610 <vipultrimbake610@gmail.com>
2024-04-09 21:59:59 +0530

    Thirfd

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,5 +4,17 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-     
+    class Product {
+        public int product(int x, int y) {
+            return 0;
+        }
+
+        public int product(int x, int y, int z) {
+            return 0;
+        }
+
+        public double product(double x, double y) {
+            return 0.0;
+        }
+    }
 }
