d91b198b2bddf93105cdb25cbfc632e3662467dd
VipulTrimbake610 <vipultrimbake610@gmail.com>
2024-04-09 21:52:15 +0530

    Thirfd

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
-
+class Product{
+    public int product(int x, int y) {
+        return 0;
+    }
+    public int product(int x, int y, int z) {
+        return 0;
+    }
+    public double product(double x, double y) {
+        return 0.0.
+                ;
+    }
+}
 public class Main {
+
+
+    public static void main(String[] args){
+        Product p = new Product();
+        p.product(10,20);
+        p.product(10,20,30);
+        p.product(9.9,9.9);
+
+    }
 
 }
